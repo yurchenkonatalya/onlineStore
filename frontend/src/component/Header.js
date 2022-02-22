1cc9fb4913e1dd0e897f924ff5063069091bc50a
Maksim <maximaa2001@gmail.com>
2022-02-23 02:10:49 +0300

    added header

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Navbar, Container, Nav, Row, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Container, Nav, Row, Col, Form, FormControl, Button } from "react-bootstrap";
 import logo from '../img/logo.ico';
 
 function Header(){
     return(
       <Navbar bg="dark" expand="dark" className="w-100">
       <Container className="d-flex justify-content-start w-100" fluid>
-      
-      <Navbar.Brand href="#home" className="ms-5 w-10">
+      <Row className="w-100">
+      <Col lg={2}>
+      <Navbar.Brand href="#home">
       <img
         src={logo}
         width="100"
@@ -16,12 +17,22 @@ function Header(){
         alt="React Bootstrap logo"
       />
     </Navbar.Brand>
-    <Form className="ms-5 w-50" >
-  <Form.Group className="w-75 d-flex justify-content-start">
-    <Form.Control  placeholder="Товар, услуга" />
-    <Button variant="outline-success" className="ms-2 w-25">Find</Button>
+    </Col>
+    <Col lg={5}>
+    <Form >
+  <Form.Group className=" d-flex justify-content-start mt-2">
+    <Form.Control className="w-75" placeholder="Товар, услуга" />
+    <Button  className="w-25"variant="outline-success">Find</Button>
   </Form.Group>
   </Form>
+  </Col>
+  <Col lg= {5}>
+  <div className="mt-2 d-flex justify-content-end">
+  <Button  variant="primary">Зарегистрироваться</Button>
+  <Button className="ms-3" variant="primary">Войти</Button>
+  </div>
+  </Col>
+  </Row>
       </Container>
     </Navbar>
   )
